e2b45c0951fad59f5165fee53e095ac418f8f042
Micael Santos Da Silva Mica <micaelsantosdigitador@gmail.com>
2022-11-22 00:58:20 -0300

    Correção de indentação e remoção a variantes

--- abertura.py
+++ abertura.py
@@ -5,20 +5,18 @@ events = ["cancelar", "saida", "fim", "Sair", "Fechar", "k", sg.WIN_CLOSED]
 janelas = {0: login(), 1: None, 2: None,
            3: None, 4: None, 5: None}
 compras = []
-contador = 0
+contador_itens = 0
 produtos = [{'Nome': 'Leite', 'Cb': '1111111111111', 'Valor': '4.56'}]
 soma = 0
 cache = ""
 #  INICIANDO PROGRAMA
 while True:
+    #  LEITURA DE EVENTOS E JANELAS
     window, event, values = sg.read_all_windows()
     # FiINALIZANDO JANELAS
     if event in events:
-        if window == janelas[0]:
+        if window == janelas[0] or window == janelas[1]:
             break
-        elif window == janelas[1]:
-            janelas[1].close()
-            janelas[0] = login()
         elif window == janelas[2]:
             janelas[2].close()
             janelas[2] = None
@@ -31,13 +29,16 @@ while True:
         elif window == janelas[5]:
             janelas[5].close()
             janelas[5] = None
-    # EVENTOS DE INICIALIZAÇÃO DE JANELAS
+
+# EVENTOS DE INICIALIZAÇÃO DE JANELAS
     if event == "avancar":
         janelas[1] = pdv()
         janelas[0].close()
+
     # INICIANDO CADASTRO DE CLIENTES
     if event == "cc" and janelas[2] is None:
         janelas[2] = cadastro_clientes()
+
     # INICIANDO JANELA DE FECHAR COMPRA
     if event == "end" and janelas[3] is None:
         janelas[3] = fecha(str(f'''{soma:.2f}''').replace('.', ','))
@@ -47,30 +48,39 @@ while True:
         janelas[1]['tot'].update(f'{tot:>140}')
         compras = []
         janelas[1]["op"].update(values=compras)
+
     # INICIANDO CADASTRO DE PRODUTOS
     if event == "add" and janelas[4] is None:
         janelas[4] = cadastro()
     if event == 'add1':
         janelas[5].close()
         janelas[4] = cadastro(cache)
-    # EVENTOS DO PDV
+# EVENTOS DO PDV
     # BUSCANDO PRODUTOS
     if event == "busca" and values["produto"].strip() != "":
-        window.refresh()
         a = busca(values["produto"], produtos)
+        # PRODUTO ENCONTRADO
         if a:
-            print(a["Cb"])
-            contador += 1
+            contador_itens += 1
+            # EVITANDO ERRO QUANTIDADE VAZIA
             if values["qua"].strip() == "":
-                values["qua"] = "1"
+                values["qua"] = "1.000"
+            # DEFININDO SOMA À SER EXIBIDA
             soma += float(a["Valor"]) * float(values["qua"].replace(",", "."))
+            # FORMATANDO VALOR (SOMA)
             tot = str(f'{soma:.2f}').replace('.', ',')
+            # ATUALIZANDO JANELA PARA O VALOR FORMATADO
             janelas[1]['tot'].update(f'{tot:>140}')
-            compras.append([contador, a["Nome"], a["Valor"],
+            # DEFININDO VALRORES DA TABELA
+            compras.append([contador_itens, a["Nome"], a["Valor"],
                             values["qua"].replace(",", ".")])
+            # ATUALIZANDO TABELA
             janelas[1]["op"].update(values=compras)
+        # PRODUTO NÃO ENCONTRADO
         else:
+            # DEFININDO VARIÁVEL COM CÓDIGO NÃO CADASTRADO
             cache = values["produto"]
+            # INICIANDO JANELA DE ERRO
             janelas[5] = non()
     # CADASTRANDO PRODUTOS !!!ALTERAR EM BREVE!!! DB EM XLSX
     if event == 'pronto' and values['nome'] != '' and values['cb'] != '' and values['valor'] != '':
