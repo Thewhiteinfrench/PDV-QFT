78bb315d3ad427585dfa6a2a0e9877b6f1cbcd84
Micael Santos Da Silva <micaelsantosdigitador@gmail.com>
2023-08-30 12:24:49 -0300

    Update Janelas

--- abertura.py
+++ abertura.py
@@ -2,14 +2,16 @@ from PDV_func import *
 from Janelas import *
 
 produtos_passados = {}
-events = ["cancelar", "Escape:27", "fim", "Sair", "Fechar", "k", sg.WIN_CLOSED]
+events = ["cancelar", "Escape:27", "fim", "Fechar"'', sg.WIN_CLOSED]
 janelas = {0: login(), 1: None, 2: None,
            3: None, 4: None, 5: None}
 compras = []
 contador_itens = 0
-produtos = [{'Nome': 'Leite', 'Cb': '1212', 'Valor': '4.56'}]
+produtos = [{'Nome': 'Leite', 'Cb': '1212', 'Valor': '4.56', "Quantidade": 0}]
 soma = 0
+pgmt = None
 cache = ""
+user_auth = False
 #  INICIANDO PROGRAMA
 while True:
     # LEITURA DE EVENTOS E JANELAS
@@ -25,7 +27,7 @@ while True:
         elif window == janelas[3]:
             if values["troco"] == "0,00":
                 validacao = sg.PopupYesNo("Quer mesmo cancelar a ação?\n"
-                                          "Todas as informações serão perdidas.",
+                                          "A compra não foi finalizada.",
                                           font="Verdana 8 bold", button_color="#feb403",
                                           background_color="#252525")
                 if validacao == "Yes":
@@ -45,13 +47,43 @@ while True:
 
     # EVENTOS DE INICIALIZAÇÃO DE JANELAS
     if event == "avancar":
-        janelas[1] = pdv()
-        janelas[0].close()
+        with open("auth.txt", "r") as autenticar:
+            data = autenticar.read()
+            if values["log"] == data.split(" - ")[0] and values["password"] == data.split(" - ")[1]:
+                user_auth = True
+                janelas[1] = pdv()
+                janelas[0].close()
+            else:
+                janelas[0]["vf"].update("Informação inválida")
+    if window == janelas[1] and user_auth:
+        # INICIANDO CADASTRO DE CLIENTES
+        if event == "F2:113" and janelas[2] is None:
+            janelas[2] = cadastro_clientes()
 
-    # INICIANDO CADASTRO DE CLIENTES
-    if event == "F2:113" and janelas[2] is None:
-        janelas[2] = cadastro_clientes()
+        # INICIANDO JANELA DE FECHAR COMPRA
+        if event == "F3:114" and janelas[3] is None and len(compras) > 0:
+            janelas[1]["qua"].update("1,000")
+            janelas[1]["pu"].update("0,00")
+            janelas[1]["tt"].update("0,00")
+            janelas[1]["descricao"].update("Descrição")
+            janelas[3] = fecha(str(f'{soma:.2f}').replace('.', ','))
+            soma = 0
+            contador_itens = 0
+            tot = str(f'{soma:.2f}').replace('.', ',')
+            janelas[1]['tot'].update(f'{tot}')
+            compras = []
+            janelas[1]["op"].update(values=compras)
 
+<<<<<<< Updated upstream
+        # INICIANDO CADASTRO DE PRODUTOS
+        if event == "F4:115" and janelas[4] is None:
+            janelas[4] = cadastro()
+=======
+<<<<<<< HEAD
+        # INICIANDO CADASTRO DE PRODUTOS
+        if event == "F4:115" and janelas[4] is None:
+            janelas[4] = cadastro()
+=======
     # INICIANDO JANELA DE FECHAR COMPRA
     if event == "F3:114" and janelas[3] is None and len(compras) > 0:
         janelas[1]["qua"].update("1,000")
@@ -61,6 +93,7 @@ while True:
         janelas[3] = fecha(str(f'{soma:.2f}').replace('.', ','))
         soma = 0
         contador = 0
+        contador_itens = 0
         tot = str(f'{soma:.2f}').replace('.', ',')
         janelas[1]['tot'].update(f'{tot}')
         compras = []
@@ -69,6 +102,8 @@ while True:
     # INICIANDO CADASTRO DE PRODUTOS
     if event == "F4:115" and janelas[4] is None:
         janelas[4] = cadastro()
+>>>>>>> cc437e0ddf62ca040c26a1d482aafefffeee519c
+>>>>>>> Stashed changes
     if event == 'add1':
         janelas[5].close()
         janelas[4] = cadastro(cache)
@@ -119,15 +154,30 @@ while True:
         produtos[0]['Nome'] = values['nome']
         produtos[0]['Cb'] = values['cb']
         produtos[0]['Valor'] = values['valor']
-        for k, v in produtos[0].items():
-            print(f"{k, v}", end="")
     # FECHANDO A COMPRA
     quant = [[]]
-    if event == 'Calcular' and values['pago'].strip() != '':
-        # FACILITADOR DE TROCO
-        compra = float(f"{values['compra']}".replace(",", "."))
-        pago = float(f"{values['pago']}".replace(",", "."))
-        janelas[3]["troco"].update(f"{pago - compra:.2f}".replace(".", ","))
-        a = troco(compra, pago)
-        janelas[3]["tb"].update(values=a)
-        continue
+    if window == janelas[3]:
+        if event == "Dinheiro":
+            janelas[3]["Dinheiro"].update(button_color="#feb403")
+            janelas[3]["Cartão"].update(button_color="#252525")
+            janelas[3]["Outros"].update(button_color="#252525")
+            pgmt = 0
+        if event == "Cartão":
+            janelas[3]["Dinheiro"].update(button_color="#252525")
+            janelas[3]["Cartão"].update(button_color="#feb403")
+            janelas[3]["Outros"].update(button_color="#252525")
+            pgmt = 1
+        if event == "Outros":
+            janelas[3]["Dinheiro"].update(button_color="#252525")
+            janelas[3]["Cartão"].update(button_color="#252525")
+            janelas[3]["Outros"].update(button_color="#feb403")
+            pgmt = 2
+
+        if pgmt == 0 and event == 'Calcular' and values['pago'].strip() != '':
+            # FACILITADOR DE TROCO
+            compra = float(f"{values['compra']}".replace(",", "."))
+            pago = float(f"{values['pago']}".replace(",", "."))
+            janelas[3]["troco"].update(f"{pago - compra:.2f}".replace(".", ","))
+            a = troco(compra, pago)
+            janelas[3]["tb"].update(values=a)
+            continue
